a57cabec80733eeef8eea35663cba91d29171bef
h-hirai <hirai.hajime@gmail.com>
2014-03-23 15:32:00 +0900

    define interfaces

---
+++ src/HomMad.hs
@@ -0,0 +1,35 @@
+module HomMad where
+
+-- |Status of a point on a board.
+data Color
+    = B -- ^Black
+    | W -- ^White
+    | E -- ^Empty
+    | O -- ^Out Of Boad
+    deriving (Show, Eq)
+
+type Board = [[Color]]
+
+type Point = (Int, Int)
+
+data GameStatus
+    = GameStatus {
+        _board :: Board          -- ^Board status
+      , _turn :: Color           -- ^Turn
+      , _prisonersB :: Int       -- ^Captured by Black
+      , _prisonersW :: Int       -- ^Captured by White
+      , _ko :: Maybe Point       -- ^Ko
+      } deriving (Show, Eq)
+
+
+boardRef :: Board -> Point -> Color
+boardRef = undefined
+
+boardPut :: Color -> Board -> Point -> Board
+boardPut = undefined
+
+canPut :: GameStatus -> Point -> Bool
+canPut = undefined
+
+putStone :: GameStatus -> Point -> GameStatus
+putStone = undefined
