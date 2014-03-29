1249e0005a7b57edd0986bfc53a692f2bc023add
h-hirai <hirai.hajime@gmail.com>
2014-03-30 01:05:45 +0900

    resize testBoard

--- test/HomMadSpec.hs
+++ test/HomMadSpec.hs
@@ -6,21 +6,25 @@ import qualified Data.Set as S
 
 spec :: Spec
 spec = do
-  let testBoard = [[E,E,E,E,E]
-                  ,[E,W,W,W,E]
-                  ,[E,W,B,W,E]
-                  ,[E,B,E,B,E]
-                  ,[E,B,B,B,E]]
+  let testBoard = [[E,E,E,E,E,E,E,E,E]
+                  ,[E,E,E,E,E,E,E,E,E]
+                  ,[E,E,E,E,E,E,E,E,E]
+                  ,[E,E,E,E,E,E,E,E,E]
+                  ,[E,E,E,E,E,E,E,E,E]
+                  ,[E,W,W,W,E,E,E,E,E]
+                  ,[E,W,B,W,E,E,E,E,E]
+                  ,[E,B,E,B,E,E,E,E,E]
+                  ,[E,B,B,B,E,E,E,E,E]]
   describe "getChain" $ do
-    let chain1 = getChain testBoard (2,2)
+    let chain1 = getChain testBoard (6,2)
     it "has alone stone" $ do
-      _chainPoints chain1  `shouldBe` S.fromList [(2,2)]
+      _chainPoints chain1  `shouldBe` S.fromList [(6,2)]
     it "has a liberty" $ do
-      _chainLiberties chain1 `shouldBe` S.fromList [(3,2)]
-    let chain2 = getChain testBoard (3,3)
+      _chainLiberties chain1 `shouldBe` S.fromList [(7,2)]
+    let chain2 = getChain testBoard (7,3)
     it "has many stones" $ do
       _chainPoints chain2
-           `shouldBe` S.fromList [(3,1),(3,3),(4,1),(4,2),(4,3)]
+           `shouldBe` S.fromList [(7,1),(7,3),(8,1),(8,2),(8,3)]
     it "has liberties" $ do
       _chainLiberties chain2
-           `shouldBe` S.fromList [(3,0),(3,2),(3,4),(4,0),(4,4)]
+           `shouldBe` S.fromList [(7,0),(7,2),(7,4),(8,0),(8,4)]
