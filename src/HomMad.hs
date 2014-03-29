3016156a4bf43d0c1a7c8138fad1baee264eeeda
h-hirai <hirai.hajime@gmail.com>
2014-03-29 14:20:16 +0900

    changed code layout only

--- src/HomMad.hs
+++ src/HomMad.hs
@@ -4,40 +4,33 @@ boardSize :: Int
 boardSize = 9
 
 -- |Status of a point on a board.
-data Color
-    = B -- ^Black
-    | W -- ^White
-    | E -- ^Empty
-    | O -- ^Out Of Boad
+data Color = B -- ^Black
+           | W -- ^White
+           | E -- ^Empty
+           | O -- ^Out Of Boad
     deriving (Show, Eq)
 
 type Board = [[Color]]
 
 type Point = (Int, Int)
 
-data GameStatus
-    = GameStatus {
-        _board :: Board          -- ^Board status
-      , _turn :: Color           -- ^Turn
-      , _prisonersB :: Int       -- ^Captured by Black
-      , _prisonersW :: Int       -- ^Captured by White
-      , _ko :: Maybe Point       -- ^Ko
-      } deriving (Show, Eq)
+data GameStatus = GameStatus {
+      _board :: Board          -- ^Board status
+    , _turn :: Color           -- ^Turn
+    , _prisonersB :: Int       -- ^Captured by Black
+    , _prisonersW :: Int       -- ^Captured by White
+    , _ko :: Maybe Point       -- ^Ko
+    } deriving (Show, Eq)
+
+
+emptyBoard :: Board
+emptyBoard = replicate boardSize $ replicate boardSize E
 
 -- |
 -- >>> boardRef emptyBoard (1,-1)
 -- O
 -- >>> boardRef emptyBoard (boardSize,1)
 -- O
--- >>> boardRef (boardPut B emptyBoard (3,3)) (3,3)
--- B
--- >>> boardRef (boardPut W emptyBoard (3,3)) (3,3)
--- W
--- >>> boardRef (boardPut W emptyBoard (3,3)) (3,2)
--- E
-
-emptyBoard :: Board
-emptyBoard = replicate boardSize $ replicate boardSize E
 
 boardRef :: Board -> Point -> Color
 boardRef b (row, col) | row < 0 = O
@@ -45,6 +38,14 @@ boardRef b (row, col) | row < 0 = O
                       | col < 0 = O
                       | col >= boardSize = O
                       | otherwise = b !! row !! col
+
+-- |
+-- >>> boardRef (boardPut B emptyBoard (3,3)) (3,3)
+-- B
+-- >>> boardRef (boardPut W emptyBoard (3,3)) (3,3)
+-- W
+-- >>> boardRef (boardPut W emptyBoard (3,3)) (3,2)
+-- E
 
 boardPut :: Color -> Board -> Point -> Board
 boardPut c b (row, col) = rplcIdx b row $ rplcIdx (b!!row) col c
