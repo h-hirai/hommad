99c492d19f5ce34d97218da3b4a1255c0c0e5117
h-hirai <hirai.hajime@gmail.com>
2014-05-09 23:30:06 +0900

    temporal commit: changing Chain

--- test/HomMad/AISpec.hs
+++ test/HomMad/AISpec.hs
@@ -17,57 +17,81 @@ makeBoard = IM.fromList . map g . filter f . zip [0..]
 
 spec :: Spec
 spec = do
-  let testBoardA = makeBoard [E,E,W,E,W,W,E,W,E
-                             ,E,E,W,W,W,E,W,W,W
-                             ,E,E,B,B,E,E,E,E,E
-                             ,E,E,B,E,B,B,E,E,E
-                             ,E,E,E,B,E,B,E,E,E
-                             ,E,E,E,B,B,B,E,E,E
-                             ,B,B,B,E,E,E,E,E,E
-                             ,B,E,B,E,E,E,E,E,W
-                             ,E,B,E,E,E,E,E,W,E]
+  let testStatusA = foldl putStone initGame [
+                     (2, 2) -- B
+                    ,(0, 2) -- W
+                    ,(2, 3) -- B
+                    ,(0, 4) -- W
+                    ,(3, 2) -- B
+                    ,(0, 5) -- W
+                    ,(3, 3) -- B
+                    ,(0, 7) -- W
+                    ,(3, 4) -- B
+                    ,(1, 2) -- W
+                    ,(4, 3) -- B
+                    ,(1, 3) -- W
+                    ,(4, 5) -- B
+                    ,(1, 4) -- W
+                    ,(5, 3) -- B
+                    ,(1, 6) -- W
+                    ,(5, 4) -- B
+                    ,(1, 7) -- W
+                    ,(5, 5) -- B
+                    ,(1, 8) -- W
+                    ,(6, 0) -- B
+                    ,(7, 8) -- W
+                    ,(6, 1) -- B
+                    ,(8, 7) -- W
+                    ,(6, 2) -- B
+                    ,(3, 8) -- W
+                    ,(7, 0) -- B
+                    ,(4, 8) -- W
+                    ,(7, 2) -- B
+                    ,(5, 8) -- W
+                    ,(8, 1) -- B
+                    ]
 
   describe "isEye" $ do
     it "is simple eye" $ do
-      isSimpleEye testBoardA Black (4,4) `shouldBe` True
+      isSimpleEye testStatusA Black (4,4) `shouldBe` True
     it "is not eye of white" $ do
-      isSimpleEye testBoardA White (4,4) `shouldBe` False
+      isSimpleEye testStatusA White (4,4) `shouldBe` False
     it "not empty point is not eye" $ do
-      isSimpleEye testBoardA White (7,8) `shouldBe` False
+      isSimpleEye testStatusA White (7,8) `shouldBe` False
     it "is not simple eye" $ do
-      isSimpleEye testBoardA Black (3,3) `shouldBe` False
+      isSimpleEye testStatusA Black (3,3) `shouldBe` False
     it "is not combined eye" $ do
-      isSimpleEye testBoardA Black (3,3) `shouldBe` False
+      isSimpleEye testStatusA Black (3,3) `shouldBe` False
     it "is not treated as combined eye " $ do
-      isCombinedEye testBoardA White (0,6) `shouldBe` False
+      isCombinedEye testStatusA White (0,6) `shouldBe` False
     it "is combined eye" $ do
-      isCombinedEye testBoardA Black (8,0) `shouldBe` True
+      isCombinedEye testStatusA Black (8,0) `shouldBe` True
 
-  let testBoardB = makeBoard [W,W,W,W,W,E,B,W,W
-                             ,W,E,W,W,E,W,W,B,W
-                             ,W,W,W,E,W,W,B,B,W
-                             ,B,B,B,W,W,B,B,W,W
-                             ,B,B,B,B,B,B,B,W,W
-                             ,B,B,B,B,B,W,W,W,E
-                             ,B,B,B,B,W,W,W,E,W
-                             ,B,E,B,B,W,W,W,W,E
-                             ,E,B,E,B,W,E,W,W,W]
+  -- let testBoardB = makeBoard [W,W,W,W,W,E,B,W,W
+  --                            ,W,E,W,W,E,W,W,B,W
+  --                            ,W,W,W,E,W,W,B,B,W
+  --                            ,B,B,B,W,W,B,B,W,W
+  --                            ,B,B,B,B,B,B,B,W,W
+  --                            ,B,B,B,B,B,W,W,W,E
+  --                            ,B,B,B,B,W,W,W,E,W
+  --                            ,B,E,B,B,W,W,W,W,E
+  --                            ,E,B,E,B,W,E,W,W,W]
 
-  describe "isSimpleEye" $ do
-    it "is not SimpleEye" $ do
-      isSimpleEye testBoardB White (0,5) `shouldBe` False
-    it "is not CombinedEye" $ do
-      isCombinedEye testBoardB White (0,5) `shouldBe` False
+  -- describe "isSimpleEye" $ do
+  --   it "is not SimpleEye" $ do
+  --     isSimpleEye testBoardB White (0,5) `shouldBe` False
+  --   it "is not CombinedEye" $ do
+  --     isCombinedEye testBoardB White (0,5) `shouldBe` False
 
-  describe "pointsCanPut" $ do
-    it "is finish" $ do
-      pointsCanPut initGame{_board=testBoardB, _turn=Black} `shouldBe` []
-    it "is last 1" $ do
-      pointsCanPut initGame{_board=testBoardB, _turn=White} `shouldBe` [(0,5)]
+  -- describe "pointsCanPut" $ do
+  --   it "is finish" $ do
+  --     pointsCanPut initGame{_board=testBoardB, _turn=Black} `shouldBe` []
+  --   it "is last 1" $ do
+  --     pointsCanPut initGame{_board=testBoardB, _turn=White} `shouldBe` [(0,5)]
 
-  let testBoardC =
-          _board $ putStone initGame{_board=testBoardB, _turn=White} (0,5)
+  -- let testBoardC =
+  --         _board $ putStone initGame{_board=testBoardB, _turn=White} (0,5)
 
-  describe "count" $ do
-    it "is (32,49)" $ do
-      count testBoardC `shouldBe` (4+4+4+5+7+5+2+1+0, 5+5+5+4+2+4+7+8+9)
+  -- describe "count" $ do
+  --   it "is (32,49)" $ do
+  --     count testBoardC `shouldBe` (4+4+4+5+7+5+2+1+0, 5+5+5+4+2+4+7+8+9)
--- test/HomMad/GobanSpec.hs
+++ test/HomMad/GobanSpec.hs
@@ -17,34 +17,39 @@ makeBoard syms = IM.fromList $ map g $ filter f $ zip [0..] syms
 
 spec :: Spec
 spec = do
-  let testBoard = makeBoard [E,W,E,E,E,E,E,E,E
-                            ,W,E,E,E,E,E,E,E,E
-                            ,E,E,E,E,E,E,E,E,E
-                            ,E,E,E,E,E,E,E,E,E
-                            ,E,E,E,E,E,E,E,E,E
-                            ,E,W,W,W,E,E,E,E,E
-                            ,E,W,B,W,E,E,E,E,E
-                            ,E,B,E,B,E,E,E,E,E
-                            ,E,B,B,B,E,E,E,E,E]
+  let testStatus = foldl putStone initGame [
+                    (6, 2) -- B
+                   ,(0, 1) -- W
+                   ,(7, 1) -- B
+                   ,(1, 0) -- W
+                   ,(7, 3) -- B
+                   ,(5, 1) -- W
+                   ,(8, 1) -- B
+                   ,(5, 2) -- W
+                   ,(8, 2) -- B
+                   ,(5, 3) -- W
+                   ,(8, 3) -- B
+                   ,(6, 1) -- W
+                   ,(8, 8) -- B
+                   ,(6, 3) -- W
+                   ]
 
   describe "getChain" $ do
-    let chain1 = getChain testBoard (6,2)
+    let chain1 = getChain testStatus (6,2)
     it "has alone stone" $ do
       _chainCoords chain1  `shouldBe` S.fromList [(6,2)]
     it "has a liberty" $ do
-      _chainLiberties chain1 `shouldBe` S.fromList [(7,2)]
-    let chain2 = getChain testBoard (7,3)
+      liberties (_board testStatus) chain1 `shouldBe` S.fromList [(7,2)]
+    let chain2 = getChain testStatus (7,3)
     it "has many stones" $ do
       _chainCoords chain2
            `shouldBe` S.fromList [(7,1),(7,3),(8,1),(8,2),(8,3)]
     it "has liberties" $ do
-      _chainLiberties chain2
+      liberties (_board testStatus) chain2
            `shouldBe` S.fromList [(7,0),(7,2),(7,4),(8,0),(8,4)]
-    it "has opponents" $ do
-      _chainOpponents chain2 `shouldBe` S.fromList [(6,1),(6,3)]
 
   describe "canPut" $ do
-    let testStatusB = GameStatus testBoard White Nothing emptyBoard IM.empty
+    let testStatusB = pass testStatus
     it "returns False for on a stone" $ do
       canPut testStatusB (5,1) `shouldBe` False
     it "returns False for out of a board" $ do
